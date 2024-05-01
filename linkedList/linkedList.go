fa8d0f8f35321a511f8fe332aa9c489d2eb8e6ef
Prajjwal <prajjwalchale18@gmail.com>
2024-05-01 16:48:56 +0530

    adding datastructrues packages

---
+++ linkedList/linkedList.go
@@ -0,0 +1,34 @@
+package linkedList
+
+import "fmt"
+
+type Node struct {
+	Data int
+	Next *Node
+}
+
+func CreateLinkedList(data []int) *Node {
+	var head *Node
+	for _, value := range data {
+		newNode := &Node{Data: value}
+		if head == nil {
+			head = newNode
+		} else {
+			curr := head
+			for curr.Next != nil {
+				curr = curr.Next
+			}
+			curr.Next = newNode
+		}
+	}
+	return head
+}
+
+func PrintLinkedList(head *Node) {
+	curr := head
+	for curr != nil {
+		fmt.Print(curr.Data, " -> ")
+		curr = curr.Next
+	}
+	fmt.Print("nil\n")
+}
