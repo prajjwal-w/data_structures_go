db2f23f6d93d7e467d54ebda22a33c3130917d28
Prajjwal <prajjwalchale18@gmail.com>
2024-05-01 15:17:25 +0530

    adding double and single linkedlists

---
+++ DoublyLinkedList/doublyLinkedList.go
@@ -0,0 +1,35 @@
+package doublyLinkedList
+
+import "fmt"
+
+type Node struct {
+	Data interface{}
+	Prev *Node
+	Next *Node
+}
+
+func CreateDoublyLinkedList(data []interface{}) *Node {
+	var head, tail *Node
+	for _, value := range data {
+		newNode := &Node{Data: value}
+		if head == nil {
+			head = newNode
+			tail = newNode
+		} else {
+			tail.Next = newNode
+			newNode.Prev = tail
+			tail = newNode
+		}
+	}
+	return head
+}
+
+func Display(head *Node) {
+	curr := head
+	for curr != nil {
+		fmt.Print(curr.Data, " <-> ")
+		curr = curr.Next
+	}
+	fmt.Print("nil")
+	fmt.Println()
+}
